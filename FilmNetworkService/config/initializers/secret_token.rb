d14c19ff364483bfd03aac81aabb2159b85603b6
Michael Ishmael <michaelishmael1976@gmail.com>
2012-03-14 00:41:33 +0000

    First commit
    Committing empty Rails web service framework

---
+++ FilmNetworkService/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilmNetworkService::Application.config.secret_token = '18678d08b5edc8b36ebac02ddb889817140f45c02f6313923e7e2e106ba4cab8ae45a633723d33936e823c01cf0dd1614b8f432b1b4ea796d13d15ec6e64df3d'
